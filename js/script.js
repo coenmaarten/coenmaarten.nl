6b1cfcadb545529013884a3337b91089d60debe7
Coen de Vries <coen_de_vries99@hotmail.com>
2018-10-19 11:13:19 +0200

    Fixed Background (for Background scroll effect)

--- js/script.js
+++ js/script.js
@@ -12,16 +12,3 @@ function stickynav() {
     main.classList.remove("main-fix");
   }
 }
-$(document).ready(function(){
-    var $win = $(window);
-
-    $('div.background').each(function(){
-        var scroll_speed = 10;
-        var $this = $(this);
-        $(window).scroll(function() {
-            var bgScroll = -(($win.scrollTop() - $this.offset().top)/ scroll_speed);
-            var bgPosition = 'center '+ bgScroll + 'px';
-            $this.css({ backgroundPosition: bgPosition });
-        });
-    });
-});
